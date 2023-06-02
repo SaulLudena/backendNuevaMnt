db14b86b4d83f475dfb5573e1450324885b7984c
SaulLudena <cornejo758@gmail.com>
2023-06-02 17:00:50 -0500

    updating file upload method

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.listen(PORT, () =>
 //middlewares---------------------------------------------------------
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
